9d416364f4a0b4259f5d595047ea095416dae1ad
Red-K4tana <ceptor.nill@gmail.com>
2022-06-21 12:37:30 +0300

    global_changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,28 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useEffect} from 'react';
+import {Container} from "./components/Container/Container";
+import {useAppDispatch, useAppSelector} from "./store/store";
+import {isAuthUser} from "./store/reducers/profile-reducer";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    const isInitializedContent = useAppSelector(state => state.profile.helpers.initializedContent)
+
+
+    const dispatch = useAppDispatch()
+
+    useEffect(() => {
+        if (!isInitializedContent) {
+            dispatch(isAuthUser())
+        }
+    }, [dispatch, isInitializedContent])
+
+    return (
+        <>
+            {
+                isInitializedContent && <Container/>
+            }
+        </>
+    )
 }
 
 export default App;
